f205f139c453ce27e059769f4a32c4e916dae7b0
woongpang <jr.woong@gmail.com>
2023-05-23 11:43:26 +0900

    comments serializer 추가

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,1 +1,19 @@
 from rest_framework import serializers
+from articles.models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
+    def get_user(self, obj):
+        return obj.user.username
+
+    class Meta:
+        model = Comment
+        exclude = ("article",)
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ("comment",)
