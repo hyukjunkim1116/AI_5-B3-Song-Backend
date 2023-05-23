f98e56701b71e257e335b120478cdfbfb89f5227
woongpang <jr.woong@gmail.com>
2023-05-23 11:43:02 +0900

    comments url 경로 추가

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from articles import views
 
 urlpatterns = [
-
+    path(
+        "<int:article_id>/comments/", views.CommentsView.as_view(), name="comment_view"
+    ),
+    path(
+        "<int:article_id>/comments/<int:comment_id>/",
+        views.CommentsDetailView.as_view(),
+        name="comments_detail_view",
+    ),
 ]
