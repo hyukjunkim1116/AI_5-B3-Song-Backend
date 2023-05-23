01a259569cd7221bb7c76fd1027e6cc2d97ba1bb
woongpang <jr.woong@gmail.com>
2023-05-23 11:45:32 +0900

    article과 user간의 foreingkey를 위해서 Users 내 user모델 추가

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField("제목", max_length=100)
+    content = models.TextField("내용")
+    created_at = models.DateTimeField("생성시간", auto_now_add=True)
+    updated_at = models.DateTimeField("마지막 수정시간", auto_now=True)
+
+    def __str__(self):
+        return str(self.title)
+
+    def like_count(self):
+        return self.like.count()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name="comments"
+    )
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.comment)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
 
 # Create your models here.
+class User(AbstractBaseUser):
+    username = models.CharField("유저 아이디", max_length=20, unique=True)
+    email = models.EmailField("이메일", max_length=255, unique=True)
+    name = models.CharField("이름", max_length=20, blank=True, null=True)
+    introduction = models.CharField("자기 소개", max_length=255, blank=True, null=True)
+
+    followings = models.ManyToManyField(
+        "self", symmetrical=False, related_name="followers", blank=True
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
