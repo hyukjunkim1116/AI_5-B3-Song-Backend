d47c5f41a578f3476a7f8073045e2a67307c0c0e
woongpang <jr.woong@gmail.com>
2023-05-24 11:03:20 +0900

    :sparkles:Feat: 새로운 기능 추가: feature/comments_crud

--- articles/urls.py
+++ articles/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
         "<int:article_id>/comments/", views.CommentsView.as_view(), name="comment_view"
     ),
     path(
-        "<int:article_id>/comments/<int:comment_id>/",
+        "comments/<int:comment_id>/",
         views.CommentsDetailView.as_view(),
         name="comments_detail_view",
     ),
--- articles/views.py
+++ articles/views.py
@@ -121,7 +121,7 @@ class ArticlePhotos(APIView):
 class CommentsView(APIView):
     def get(self, request, article_id):
         """댓글 보기"""
-        articles = Article.objects.get(id=article_id)
+        articles = get_object_or_404(Article, id=article_id)
         comments = articles.comments.all()
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -137,7 +137,7 @@ class CommentsView(APIView):
 
 
 class CommentsDetailView(APIView):
-    def put(self, request, article_id, comment_id):
+    def put(self, request, comment_id):
         """댓글 수정"""
         comment = get_object_or_404(Comment, id=comment_id)
         if request.user == comment.user:
@@ -150,7 +150,7 @@ class CommentsDetailView(APIView):
         else:
             return Response("권한이 없습니다!", status=status.HTTP_403_FORBIDDEN)
 
-    def delete(self, request, article_id, comment_id):
+    def delete(self, request, comment_id):
         """댓글 삭제"""
         comment = get_object_or_404(Comment, id=comment_id)
         if request.user == comment.user:
