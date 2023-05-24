4a12e9616f3b6447121f984627540791e0a7c1a6
hyukjunkim1116 <nmkk1234@naver.com>
2023-05-24 15:37:28 +0900

    append slash

--- users/urls.py
+++ users/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("profile/<int:user_id>/", views.ProfileView.as_view(), name="profile_view"),
-    path("<int:user_id>/photos", views.UserPhotoView.as_view()),
+    path("<int:user_id>/photos/", views.UserPhotoView.as_view()),
 ]
