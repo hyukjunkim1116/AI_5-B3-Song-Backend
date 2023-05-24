727936253f6d91be132d8adc5e13fd22afcb98c5
myo-05 <gzf05@naver.com>
2023-05-24 16:25:29 +0900

    :sparkles:Feat: profile RUD #1 #2

--- articles/urls.py
+++ articles/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.Articles.as_view()),
-    path("<int:article_id>", views.ArticleDetail.as_view()),
-    path("<int:article_id>/photos", views.ArticlePhotos.as_view()),
+    path("<int:article_id>/", views.ArticleDetail.as_view()),
+    path("<int:article_id>/photos/", views.ArticlePhotos.as_view()),
     path(
         "<int:article_id>/comments/", views.CommentsView.as_view(), name="comment_view"
     ),
--- users/urls.py
+++ users/urls.py
@@ -6,9 +6,10 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
+    path("", views.UserView.as_view(), name="user_all_view"),
     path("signup/", views.UserView.as_view(), name="user_view"),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("profile/<int:user_id>/", views.ProfileView.as_view(), name="profile_view"),
-    path("<int:user_id>/photos", views.UserPhotoView.as_view()),
+    path("<int:user_id>/photos/", views.UserPhotoView.as_view()),
 ]
