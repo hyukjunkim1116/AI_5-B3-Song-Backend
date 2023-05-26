cfc9323013747fa8554022463a864f3b2f9ddae3
woongpang <jr.woong@gmail.com>
2023-05-26 15:44:10 +0900

    🐛Fix: 버그 수정: openai가 뮤직 영상만 찾을수 있도록 수정 #21

--- articles/views.py
+++ articles/views.py
@@ -53,54 +53,54 @@ class Articles(APIView):
         )
         return Response(serializer.data)
 
-    # def post(self, request):
-    #     """게시글 작성하기"""
-    #     serializer = ArticleDetailSerializer(
-    #         data=request.data,
-    #         # context={"request": request},
-    #     )
-    #     if serializer.is_valid():
-    #         try:
-    #             article = serializer.save(owner=request.user)
-    #             serializer = ArticleDetailSerializer(article)
-    #             return Response(serializer.data)
-    #         except Exception as e:
-    #             print(e)
-    #     else:
-    #         return Response(
-    #             serializer.errors,
-    #             status=HTTP_400_BAD_REQUEST,
-    #         )
-
     def post(self, request):
-        serializer = ArticleDetailSerializer(data=request.data)
+        """게시글 작성하기"""
+        serializer = ArticleDetailSerializer(
+            data=request.data,
+            # context={"request": request},
+        )
+        if serializer.is_valid():
+            try:
+                article = serializer.save(owner=request.user)
+                serializer = ArticleDetailSerializer(article)
+                return Response(serializer.data)
+            except Exception as e:
+                print(e)
+        else:
+            return Response(
+                serializer.errors,
+                status=HTTP_400_BAD_REQUEST,
+            )
 
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+    # def post(self, request):
+    #     serializer = ArticleDetailSerializer(data=request.data)
 
-        try:
-            article = serializer.save(owner=request.user)
-            content = request.data["content"]
+    #     if not serializer.is_valid():
+    #         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
-            recommendation, youtube_link = recommend_music_and_link(content)
-            link_comment = f"이런 음악 어떠세요?\n{recommendation} - {youtube_link}"
+    #     try:
+    #         article = serializer.save(owner=request.user)
+    #         content = request.data["content"]
 
-            serializer = ArticleDetailSerializer(article)
+    #         recommendation, youtube_link = recommend_music_and_link(content)
+    #         link_comment = f"이런 음악 어떠세요?\n{recommendation} - {youtube_link}"
 
-            url = f"{settings.API_BASE_URL}/articles/{article.id}/comments/"
-            data = {"comment": link_comment}
-            headers = {"Authorization": f"Bearer {request.auth.__str__()}"}
-            response = requests.post(url, json=data, headers=headers)
+    #         serializer = ArticleDetailSerializer(article)
 
-            if response.status_code != status.HTTP_200_OK:
-                print(
-                    "포스팅 오류",
-                    response.status_code,
-                )
+    #         url = f"{settings.API_BASE_URL}/articles/{article.id}/comments/"
+    #         data = {"comment": link_comment}
+    #         headers = {"Authorization": f"Bearer {request.auth.__str__()}"}
+    #         response = requests.post(url, json=data, headers=headers)
 
-            return Response(serializer.data)
-        except Exception as e:
-            return JsonResponse({"detail": str(e)}, status=500)
+    #         if response.status_code != status.HTTP_200_OK:
+    #             print(
+    #                 "포스팅 오류",
+    #                 response.status_code,
+    #             )
+
+    #         return Response(serializer.data)
+    #     except Exception as e:
+    #         return JsonResponse({"detail": str(e)}, status=500)
 
 
 class ArticleDetail(APIView):
