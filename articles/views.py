b435f08b1336ab777324625e076cb3d45be7f2ce
Kim Gwang woon <misili304@naver.com>
2023-05-29 12:59:31 +0900

    :ambulance:HOTFIX: kakaologin url, unused module delete #1

--- articles/views.py
+++ articles/views.py
@@ -10,8 +10,6 @@ from rest_framework.status import (
 )
 from rest_framework.exceptions import (
     NotFound,
-    NotAuthenticated,
-    ParseError,
     PermissionDenied,
 )
 from .serializers import (
