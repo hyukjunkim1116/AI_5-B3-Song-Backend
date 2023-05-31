94a4ed78775a42b54d785d830649b372ea9ad925
hyukjunkim1116 <nmkk1234@naver.com>
2023-05-31 18:18:05 +0900

    allowed host

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ CF_TOKEN = my_settings.CF_TOKEN["CF_TOKEN"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -129,6 +129,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
