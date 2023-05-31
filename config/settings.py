620b0b78a4429e0596ec75a1e0b5ee4ba1fc4478
hyukjunkim1116 <nmkk1234@naver.com>
2023-05-31 20:10:04 +0900

    secret_key

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = my_settings.SECRET
+SECRET_KEY = "django"
 # CF_ID = my_settings.CF_ID["CF_ID"]
 # CF_TOKEN = my_settings.CF_TOKEN["CF_TOKEN"]
 
