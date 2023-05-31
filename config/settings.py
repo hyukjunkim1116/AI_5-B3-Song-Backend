a571c925121256369e7f6bad4012f48b0fa8d187
hyukjunkim1116 <nmkk1234@naver.com>
2023-05-31 21:23:05 +0900

    postgres

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,6 @@ if POSTGRES_DB:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": POSTGRES_DB,
             "USER": os.environ.get("POSTGRES_USER", ""),
             "PASSWORD": os.environ.get("POSTGRES_PASSWORD", ""),
             "HOST": os.environ.get("POSTGRES_HOST", ""),
