b21b010c7d417e67a581110cbeec187a9fb6fd29
hyukjunkim1116 <nmkk1234@naver.com>
2023-06-03 22:57:07 +0900

    csrf

--- config/settings.py
+++ config/settings.py
@@ -214,13 +214,13 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = [
     "https://*.drinkdrinkdrink.xyz",
+    "https://drinkdrinkdrink.xyz",
     "https://www.backend.drinkdrinkdrink.xyz",
-    "*.drinkdrinkdrink.xyz",
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://*.drinkdrinkdrink.xyz",
+    "https://drinkdrinkdrink.xyz",
     "https://www.backend.drinkdrinkdrink.xyz",
-    "*.drinkdrinkdrink.xyz",
 ]
 # CSRF 허용 목록을 CORS와 동일하게 설정합니다.
 
